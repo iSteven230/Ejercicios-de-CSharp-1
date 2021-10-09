c74c9f7866a83b1df3184daa0007305b3fcfb531
itsSteven23 <75346434+itsSteven23@users.noreply.github.com>
2021-10-09 12:42:31 -0500

    tercer ejercicio 1.0

---
+++ actividad3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace actividad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int Cvendida;
+            double precio, total=0;
+            Console.WriteLine("introduzca la cantidad vendida");
+            Cvendida = int.Parse(Console.ReadLine());
+            if (Cvendida > 0)
+            {
+                Console.WriteLine("introduzca el precio");
+                precio = double.Parse(Console.ReadLine());
+                total = Cvendida * precio;
+                Console.WriteLine(total);
+            }
+            else
+            {
+                Console.WriteLine("se termina porque no hay cantidad");
+            }  
+        }
+    }
+}
