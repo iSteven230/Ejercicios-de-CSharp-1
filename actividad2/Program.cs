37104a58617cceed304c87292668b8621f522512
itsSteven23 <75346434+itsSteven23@users.noreply.github.com>
2021-10-08 23:59:08 -0500

    actualizacion

--- actividad2/Program.cs
+++ actividad2/Program.cs
@@ -9,9 +9,9 @@ namespace actividad2
             int num;
             Console.WriteLine("digite un numero para hacer un cuadra a partir de ese dato");
             num = int.Parse(Console.ReadLine());
-            for(int i = 0; i < num; i++)
+            for (int i = 0; i < num; i++)
             {
-                for(int j = 0; j < num; j++)
+                for (int j = 0; j < num; j++)
                 {
                     Console.Write(" *");
                 }
