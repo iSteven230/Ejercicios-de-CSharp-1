490d71bfc1c44162cf03e97c4c5b8d5b5aa3798d
itsSteven23 <75346434+itsSteven23@users.noreply.github.com>
2021-10-09 13:46:01 -0500

    1.1

---
+++ actividad5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace actividad5
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			int a = int.Parse(Console.ReadLine());
+			for (int y = 1; y <= a; y++)
+			{
+				for (int x = 1; x <= a - y; x++)
+				{
+					Console.Write(" ");
+				}
+
+				for (int x = 1; x <= y; x++)
+				{
+					Console.Write("*");
+				}
+
+				for (int x = 1; x <= y - 1; x++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+		}
+    }
+}
+
