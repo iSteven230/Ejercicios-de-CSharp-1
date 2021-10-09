501e69ee6faba7992473fbe8cddbc9aabf79946f
itsSteven23 <75346434+itsSteven23@users.noreply.github.com>
2021-10-08 21:42:01 -0500

    Agregar archivos de proyecto.

---
+++ actividad1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace actividad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int numeros, conta = 0, num, numM = 0;
+                Console.WriteLine("Digite cuantos numero desea ingresar: ");
+                numeros = int.Parse(Console.ReadLine());
+                while (conta < numeros)
+                {
+                    conta++;
+                    Console.WriteLine("{0} digite un numero", conta);
+                    num = int.Parse(Console.ReadLine());
+                    if (num > numM)
+                    {
+                        numM = num;
+                    }
+                }
+                Console.WriteLine("el numero mayor es: {0}", numM);
+            }
+    }
+}
